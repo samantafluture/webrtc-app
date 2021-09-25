78c4c5e7f824213a58d45ded5d526376b23123e3
Samanta Fluture <samantafluture@gmail.com>
2021-09-25 10:47:28 -0300

    upload files from workshop

---
+++ apps/webapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppProviders } from './app-providers';
+import { environment } from '../environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+
+  providers: [AppProviders.forPorts(environment)],
+
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ apps/webapp/src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment: {
+  production: boolean;
+  signaling: string;
+  iceServers: RTCIceServer[];
+} = {
+  production: false,
+  signaling: 'http://localhost:3333',
+  iceServers: [
+    {
+      urls: ['stun:54.90.98.123:3478'],
+      username: 'works',
+      credential: 'contact',
+      credentialType: 'password',
+    },
+  ],
+};
